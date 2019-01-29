1edc26e161db96f46cdc211543f01eff7d404a09
Junaid Farooq <junaid@evercam.io>
2019-01-29 12:07:48 +0500

    Added authorize plug.

---
+++ lib/evercam_admin_web/controllers/authorize.ex
@@ -0,0 +1,19 @@
+defmodule EvercamAdmin.Authorize do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  def user_check(conn, _opts) do
+    get_session(conn, :user_id)
+    |> bypass_request(conn)
+  end
+
+  def bypass_request(nil, conn), do: auth_error conn, "You need to log in to view this page", "/users/sign_in"
+  def bypass_request(_, conn), do: IO.inspect conn
+
+  def auth_error(conn, message, path) do
+    conn
+    |> put_flash(:danger, message)
+    |> redirect(to: path)
+    |> halt
+  end
+end

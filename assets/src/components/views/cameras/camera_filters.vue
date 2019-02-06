cd1a20ed8cbe154c5354b82c160c0702b6f7bddb
Junaid Farooq <junaid@evercam.io>
2019-02-06 18:20:13 +0500

    All filers are responsive now.

--- assets/src/components/views/cameras/camera_filters.vue
+++ assets/src/components/views/cameras/camera_filters.vue
@@ -1,83 +1,52 @@
 <template>
-  <div class="row overlapping">
-    <div class="form-camera-report">
-      <div class="filter-naming">
-        Search :
+  <div class="row cameras-filter_css">
+    <form>
+      <div class="form-row">
+        <div class="col-0.5 search-label">
+          <label class="control-label">Search :</label>
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="ID" id="camera-id" autocomplete="off" v-model="camera_exid" @keyup="cameraFilterGlobal">
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="Owner" id="camera-id" autocomplete="off" v-model="camera_owner" @keyup="cameraFilterGlobal">
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="Name" id="camera-id" autocomplete="off" v-model="camera_name" @keyup="cameraFilterGlobal">
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="IP" id="camera-id" autocomplete="off" v-model="camera_ip" @keyup="cameraFilterGlobal">
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="Username" id="camera-id" autocomplete="off" v-model="username" @keyup="cameraFilterGlobal">
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="Password" id="camera-id" autocomplete="off" v-model="password" @keyup="cameraFilterGlobal">
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="Model" id="camera-id" autocomplete="off" v-model="model" @keyup="cameraFilterGlobal">
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="Vendor" id="camera-id" autocomplete="off" v-model="vendor" @keyup="cameraFilterGlobal">
+        </div>
+        <div class="col">
+          <button type="button" @click="deleteCameras" class="btn btn-danger btn-right-margin">Delete Camera(s)</button>
+        </div>
       </div>
-      <div class="camera-inputs">
-        <input type="text" class="first-inps" placeholder="ID" id="camera-id" autocomplete="off" v-model="camera_exid" @keyup="cameraFilterGlobal">
-        <input type="text" class="first-inps" placeholder="Owner" id="owner" autocomplete="off" v-model="camera_owner" @keyup="cameraFilterGlobal">
-        <input type="text" class="first-inps" placeholder="Camera Name" id="camera-name" autocomplete="off" v-model="camera_name" @keyup="cameraFilterGlobal">
-        <input type="text" class="first-inps" placeholder="Camera IP" id="camera-ip" autocomplete="off" v-model="camera_ip" @keyup="cameraFilterGlobal">
-        <input type="text" class="first-inps" placeholder="Username" id="username" autocomplete="off" v-model="username" @keyup="cameraFilterGlobal">
-        <input type="text" class="first-inps" placeholder="Password" id="password" autocomplete="off" v-model="password" @keyup="cameraFilterGlobal">
-        <input type="text" class="first-inps" placeholder="Model" id="model" autocomplete="off" v-model="model" @keyup="cameraFilterGlobal">
-        <input type="text" class="first-inps" placeholder="Vendor" id="vendor" autocomplete="off" v-model="vendor" @keyup="cameraFilterGlobal">
-        <div class="chk-postion hide"><label><div class="checker" id="uniform-exclude_construction"><span><input type="checkbox" id="exclude_construction"></span></div> Exclude Constructions</label></div>
-        <button class="clear-btn-f" id="btn_delete_cameras" @click="deleteCameras">Delete Camera(s)</button>
-      </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <style scoped>
-.overlapping {
+.form-control {
+  box-shadow: none;
+  -moz-box-shadow: none;
+  -webkit-box-shadow: none;
+}
+
+.search-label {
   margin-top: 4px;
-  z-index: 1;
-  position: relative;
-  margin-left: -1px;
-}
-
-.form-camera-report {
-  width: 80%;
-  overflow: hidden;
-  height: auto;
-  float: left;
-  text-align: center;
-}
-
-.filter-naming {
-  width: 62px;
-  padding: 9px 2px;
-  height: auto;
-  float: left;
-  text-align: right;
-  font-size: 13px;
-}
-
-.camera-inputs {
-  width: 954px;
-  float: left;
-  height: auto;
-  overflow: hidden;
-  padding: 3px 1px;
-}
-
-.first-inps {
-  background-color: #eee;
-  width: 95px;
-  padding: 7px;
-  margin-left: 5px;
-  border: 0;
-  float: left;
-}
-
-.clear-btn-f {
-  padding: 7px;
-  display: inline-block;
-  background-color: #428bca;
-  float: left;
-  color: white;
-  border: 0;
-  margin-left: 10px;
-}
-
-.form-camera-report .chk-postion {
-  float: left;
-}
-
-.hide {
-  display: none !important;
+  margin-left: 15px;
 }
 </style>
 
--- assets/src/components/views/vendor_models/vendor_model_filters.vue
+++ assets/src/components/views/vendor_models/vendor_model_filters.vue
@@ -1,73 +1,28 @@
 <template>
-  <div class="row overlapping">
-    <div class="form-camera-report">
-      <div class="filter-naming">
-        Search :
+  <div class="row cameras-filter_css">
+    <form>
+      <div class="form-row">
+        <div class="col-0.5 search-label">
+          <label class="control-label">Search :</label>
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" placeholder="Vendor/Model" id="vendor/model" autocomplete="off" v-model="search" @keyup="vendorModelFilterGlobal">
+        </div>
       </div>
-      <div class="camera-inputs">
-        <input type="text" class="first-inps" placeholder="ID" id="Vendor/Model" autocomplete="off" v-model="search" @keyup="vendorModelFilterGlobal">
-      </div>
-    </div>
+    </form>
   </div>
 </template>
 
 <style scoped>
-.overlapping {
+.form-control {
+  box-shadow: none;
+  -moz-box-shadow: none;
+  -webkit-box-shadow: none;
+}
+
+.search-label {
   margin-top: 4px;
-  z-index: 1;
-  position: relative;
-  margin-left: -1px;
-}
-
-.form-camera-report {
-  width: 80%;
-  overflow: hidden;
-  height: auto;
-  float: left;
-  text-align: center;
-}
-
-.filter-naming {
-  width: 62px;
-  padding: 9px 2px;
-  height: auto;
-  float: left;
-  text-align: right;
-  font-size: 13px;
-}
-
-.camera-inputs {
-  float: left;
-  height: auto;
-  overflow: hidden;
-  padding: 3px 1px;
-}
-
-.first-inps {
-  background-color: #eee;
-  width: 95px;
-  padding: 7px;
-  margin-left: 5px;
-  border: 0;
-  float: left;
-}
-
-.clear-btn-f {
-  padding: 7px;
-  display: inline-block;
-  background-color: #428bca;
-  float: left;
-  color: white;
-  border: 0;
-  margin-left: 10px;
-}
-
-.form-camera-report .chk-postion {
-  float: left;
-}
-
-.hide {
-  display: none !important;
+  margin-left: 15px;
 }
 </style>
 

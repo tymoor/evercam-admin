9d86d0ec264d670c2ae68f797657fbf81d828cfa
Junaid Farooq <junaid@evercam.io>
2019-03-05 17:59:45 +0500

    MetaData report, work in progress.

---
+++ assets/src/components/views/meta_datas/FieldsDef.js
@@ -0,0 +1,61 @@
+export default [
+  {
+    name: 'inserted_at',
+    title: 'Created At',
+    sortField: 'inserted_at',
+    togglable: true
+  },
+  {
+    name: 'camera_link',
+    title: 'Camera',
+    sortField: 'camera_name',
+    togglable: true
+  },
+  {
+    name: 'camera_exid',
+    title: 'Camera Exid',
+    sortField: 'camera_exid',
+    togglable: true
+  },
+  {
+    name: 'requester',
+    title: 'Requester',
+    togglable: true
+  },
+  {
+    name: 'action',
+    title: 'Type',
+    sortField: 'action',
+    togglable: true
+  },
+  {
+    name: 'rtsp_url',
+    title: 'URL',
+    togglable: true
+  },
+  {
+    name: 'resolution',
+    title: 'Resolution',
+    togglable: true
+  },
+  {
+    name: 'frame_rate',
+    title: 'Frame',
+    togglable: true
+  },
+  {
+    name: 'bit_rate',
+    title: 'Bit Rate',
+    togglable: true
+  },
+  {
+    name: 'codec',
+    title: 'Codec',
+    togglable: true
+  },
+  {
+    name: 'ip',
+    title: 'IP',
+    togglable: true
+  }
+]
---
+++ assets/src/components/views/meta_datas/meta_datas.vue
@@ -0,0 +1,171 @@
+<template>
+  <div>
+    <div class="overflow-forms">
+      <v-meta-datas-filters />
+    </div>
+    <div>
+      <v-meta-datas-show-hide :vuetable-fields="vuetableFields" />
+    </div>
+
+    <v-horizontal-scroll />
+
+    <div id="table-wrapper" :class="['vuetable-wrapper ui basic segment', loading]">
+      <div class="handle">
+        <vuetable ref="vuetable"
+          api-url="/v1/meta_datas"
+          :fields="fields"
+          pagination-path=""
+          data-path="data"
+          :per-page="perPage"
+          :sort-order="sortOrder"
+          :append-params="moreParams"
+          @vuetable:pagination-data="onPaginationData"
+          @vuetable:initialized="onInitialized"
+          @vuetable:loading="showLoader"
+          @vuetable:loaded="hideLoader"
+          :css="css.table"
+        >
+        </vuetable>
+      </div>
+      <div class="vuetable-pagination ui bottom segment grid">
+        <div class="field perPage-margin">
+          <label>Per Page:</label>
+          <select class="ui simple dropdown" v-model="perPage">
+            <option :value="60">60</option>
+            <option :value="100">100</option>
+            <option :value="500">500</option>
+            <option :value="1000">1000</option>
+          </select>
+        </div>
+        <vuetable-pagination-info ref="paginationInfo"
+        ></vuetable-pagination-info>
+        <component :is="paginationComponent" ref="pagination"
+          @vuetable-pagination:change-page="onChangePage"
+        ></component>
+      </div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.perPage-margin {
+  margin-top: 5px;
+}
+
+.overflow-forms {
+  overflow: hidden;
+  width: 85%;
+}
+
+#table-wrapper {
+  margin-top: -2px;
+}
+
+.embed_code {
+  cursor: pointer;
+}
+
+</style>
+
+<script>
+import FieldsDef from "./FieldsDef.js";
+import TableWrapper from "./TableWrapper.js";
+import MetaDataFilters from "./meta_datas_filters";
+import MetaDataShowHide from "./meta_datas_show_hide";
+
+export default {
+  components: {
+    "v-meta-datas-filters": MetaDataFilters,
+    "v-meta-datas-show-hide": MetaDataShowHide
+  },
+  data: () => {
+    return {
+      paginationComponent: "vuetable-pagination",
+      loading: "",
+      vuetableFields: false,
+      perPage: 60,
+      sortOrder: [
+        {
+          field: 'inserted_at',
+          direction: 'desc',
+        }
+      ],
+      css: TableWrapper,
+      moreParams: {},
+      fields: FieldsDef,
+      vendorData: {}
+    }
+  },
+  watch: {
+    perPage(newVal, oldVal) {
+      this.$nextTick(() => {
+        this.$refs.vuetable.refresh();
+      });
+    },
+
+    paginationComponent(newVal, oldVal) {
+      this.$nextTick(() => {
+        this.$refs.pagination.setPaginationData(
+          this.$refs.vuetable.tablePagination
+        );
+      });
+    }
+  },
+
+  beforeUpdate() {
+    document.addEventListener("resize", this.setScrollBar());
+  },
+
+  mounted() {
+    this.$nextTick(function() {
+      window.addEventListener('resize', this.setScrollBar);
+      this.setScrollBar()
+      window.setInterval(() => {
+        this.syncStatMetaData()
+      }, 20000);
+    });
+    this.$events.$on('meta-datas-filter-set', eventData => this.onFilterSet(eventData))
+  },
+
+  methods: {
+    onFilterSet (filters) {
+      this.moreParams = {
+        "search": filters.search
+      }
+      this.$nextTick( () => this.$refs.vuetable.refresh())
+    },
+
+    onPaginationData(tablePagination) {
+      this.$refs.paginationInfo.setPaginationData(tablePagination);
+      this.$refs.pagination.setPaginationData(tablePagination);
+    },
+
+    onChangePage(page) {
+      this.$refs.vuetable.changePage(page);
+    },
+
+    onInitialized(fields) {
+      this.vuetableFields = fields.filter(field => field.togglable);
+    },
+
+    showLoader() {
+      this.loading = "loading";
+    },
+
+    hideLoader() {
+      this.loading = "";
+    },
+
+    syncStatMetaData() {
+      this.$http.get(`/v1/sync_stat_metadata`).then(response => {
+        console.log("i am here")
+        this.$nextTick(() => {
+          this.$refs.vuetable.refresh();
+        });
+      }, error => {
+        console.log("something went wrong.")
+      });
+    }
+  }
+}
+</script>

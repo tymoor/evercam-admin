8ceb70dc70a710ca91eab1fedb2cccee5b0a5d86
Junaid Farooq <junaid@evercam.io>
2019-03-29 16:46:20 +0500

    Fix.

--- lib/evercam_admin_web/controllers/cloud_recordings_controller.ex
+++ lib/evercam_admin_web/controllers/cloud_recordings_controller.ex
@@ -82,7 +82,7 @@ defmodule EvercamAdminWeb.CloudRecordingsController do
     Enum.reduce(params, "where 1=1", fn param, condition = _acc ->
       {name, value} = param
       cond do
-        name == "camera_name" && value != "" -> condition <> " and lower(c.name) like lower('%#{value}')"
+        name == "camera_name" && value != "" -> condition <> " and lower(c.name) like lower('%#{value}%')"
         name == "owner" && value != "" -> condition <> " and lower(u.firstname || ' ' || u.lastname) like lower('%#{value}%')"
         name == "status" && value != "" -> condition <> " and lower(status) like lower('%#{value}%')"
         name == "storage_duration" && value != "" -> condition <> " and CAST(storage_duration AS TEXT) like '#{value}%'"

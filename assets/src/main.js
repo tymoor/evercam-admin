efa7739e4d7db974dace8006b56ac538aa855cd1
Junaid Farooq <junaid@evercam.io>
2019-04-06 22:51:57 +0500

    Added iziToaster notfication as Mixin.

--- assets/src/main.js
+++ assets/src/main.js
@@ -72,7 +72,34 @@ Vue.mixin({
       document.querySelector("div.top-horizontal-scroll").style.width = tableWidth + "px";
       document.querySelector("div.top-scrollbar").style.width = tableWrapper + "px"
     }
-  }
+  },
+
+  notifications: {
+    showSuccessMsg: {
+      type: VueNotifications.types.success,
+      title: 'Hello there',
+      message: 'That\'s the success!',
+      position: "topCenter"
+    },
+    showInfoMsg: {
+      type: VueNotifications.types.info,
+      title: 'Hey you',
+      message: 'Here is some info for you',
+      position: "topCenter"
+    },
+    showWarnMsg: {
+      type: VueNotifications.types.warn,
+      title: 'Wow, man',
+      message: 'That\'s the kind of warning',
+      position: "topCenter"
+    },
+    showErrorMsg: {
+      type: VueNotifications.types.error,
+      title: 'Wow-wow',
+      message: 'That\'s the error',
+      position: "topCenter"
+    }
+  },
 })
 
 document.addEventListener('DOMContentLoaded', () => {

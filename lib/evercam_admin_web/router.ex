daa8e40ca05f7b56010aab07b550be32f3a2d7a8
Junaid Farooq <junaid@evercam.io>
2019-06-28 11:57:29 +0500

    Added Options route for Cameras endpoint.

--- lib/evercam_admin_web/router.ex
+++ lib/evercam_admin_web/router.ex
@@ -22,6 +22,7 @@ defmodule EvercamAdminWeb.Router do
     patch "/update_multiple_users", UsersController, :update_multiple_users
     get "/without_company_intercom_users", UsersController, :without_company_intercom_users
     get "/cameras", CamerasController, :index
+    options "/cameras", CamerasController, :nothing
     get "/construction_cameras", CamerasController, :construction_cameras
     get "/onvif_cameras", CamerasController, :onvif_cameras
     get "/duplicate_cameras", CamerasController, :duplicate_cameras

275c17f8a479e3812e634d5ae62d2cfdcf68f532
Junaid Farooq <junaid@evercam.io>
2019-10-08 15:26:15 +0100

    Axios in MetaData module.

--- assets/src/components/views/meta_datas/meta_datas.vue
+++ assets/src/components/views/meta_datas/meta_datas.vue
@@ -156,15 +156,23 @@ export default {
     },
 
     syncStatMetaData() {
-      this.$http.get(`/v1/sync_stat_metadata`).then(response => {
-        this.$nextTick(() => {
-          if (this.$refs.vuetable) {
-            this.$refs.vuetable.refresh();
-          }
-        });
-      }, error => {
-        console.log("something went wrong.")
-      });
+
+      axios({
+        method: 'get',
+        url: `/v1/sync_stat_metadata`,
+        data: {
+        }
+      }).then(response => {
+        if (response.status == 200) {
+          this.$nextTick(() => {
+            if (this.$refs.vuetable) {
+              this.$refs.vuetable.refresh();
+            }
+          })
+        } else {
+          console.log("something went wrong.")
+        }
+      })
     }
   }
 }
